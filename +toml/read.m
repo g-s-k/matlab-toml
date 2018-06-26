06ffc8f761aacbdcbfc1a56520bdb19858bb6f21
George Kaplan <gkaplan@nu-tek-optics.com>
2018-06-26 14:47:39 -0400

    renamed load.m to read.m

--- +toml/read.m
+++ +toml/read.m
@@ -1,11 +1,11 @@
-% LOAD parse TOML data from a file
+% READ parse TOML data from a file
 %
-%   LOAD('file.toml') loads the contents of `file.toml` and parses
+%   READ('file.toml') loads the contents of `file.toml` and parses
 %   that data into a MATLAB struct.
 %
 %   See also FILEREAD, TOML.DECODE
 
-function toml_data = load(filename)
+function toml_data = read(filename)
   raw_text = fileread(filename);
   toml_data = toml.decode(raw_text);
 end
