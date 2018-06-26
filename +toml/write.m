da5a0133123ac3d68cd22ce1807fa5b954410b22
George Kaplan <gkaplan@nu-tek-optics.com>
2018-06-26 17:02:38 -0400

    convenience function to encode and write inline

---
+++ +toml/write.m
@@ -0,0 +1,12 @@
+% WRITE serialize MATLAB data as TOML and write to file
+%
+%   WRITE('file.toml', struct('key', 5)) writes the text `key = 5` to
+%   the file `file.toml`.
+%
+%   See also TOML.ENCODE, TOML.READ
+
+function write(filename, matl_strct)
+  fid = fopen(filename, 'w');
+  fprintf(fid, toml.encode(matl_strct));
+  fclose(fid);
+end
