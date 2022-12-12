f3a5648f71d89e1d9a92297d8038c276dfda6295
George Kaplan <george@georgekaplan.xyz>
2022-12-12 20:04:28 +0100

    implement utf-8 validation

--- +toml/read.m
+++ +toml/read.m
@@ -6,6 +6,36 @@
 %   See also FILEREAD, TOML.DECODE
 
 function toml_data = read(filename)
-  raw_text = fileread(filename);
+  raw_text = '';
+
+  fid = fopen(filename, 'r');
+  while ~feof(fid)
+    c = fread(fid, 1);
+    if c < 128
+      raw_text = [raw_text, char(c)];
+      multi = [];
+    elseif c < 192
+      error('toml:InvalidUTF8', ...
+        ['Bad UTF-8 in file ' filename]);
+    else
+      raw_text = [raw_text, char(c), char(get_continuation_byte(fid))];
+      if c >= 224
+        raw_text = [raw_text, char(get_continuation_byte(fid))];
+      end
+    end
+  end
+
   toml_data = toml.decode(raw_text);
+end
+
+function b = get_continuation_byte(fid)
+  b = fread(fid, 1);
+
+  if b < 127
+    error("toml:InvalidUTF8", ...
+      ['Expected continuation byte, found ASCII character ' char(b)]);
+  elseif b > 191
+    error("toml:InvalidUTF8", ...
+      ['Expected continuation byte, found leading byte `' sprintf('%d', b) '`']);
+  end
 end
